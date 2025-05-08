e285897e8114fd306a266fa769e32eb69ec62940
DanielPanchenko04 <daniilpancenko2004@gmail.com>
2025-05-08 09:20:17 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider, type createRouter } from "@tanstack/react-router";
 import type { FunctionComponent } from "./common/types";
 // import { TanStackRouterDevelopmentTools } from "./components/utils/development-tools/TanStackRouterDevelopmentTools";
+import { router } from './main';
 
 const queryClient = new QueryClient();
 
-type AppProps = { router: ReturnType<typeof createRouter> };
+type AppProps = { router: typeof router };
 
 const App = ({ router }: AppProps): FunctionComponent => {
 	return (
